f3a59c3c7050673fa554ea331e50a1e4cd9284df
endy <7ofpentacles@gmail.com>
2023-06-07 13:25:34 +0700

    test

---
+++ libs/common/src/config/config.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import {
+  ConfigModule as NestConfigModule,
+  ConfigService,
+} from '@nestjs/config';
+import * as Joi from 'joi';
+
+@Module({
+  imports: [
+    // validation properties from .env for database configuration
+    NestConfigModule.forRoot({
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+      }),
+    }),
+  ],
+  providers: [ConfigService],
+  exports: [ConfigService],
+})
+export class ConfigModule {}
